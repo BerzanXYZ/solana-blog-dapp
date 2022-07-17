8557f7c0238d92e4c66008b6b43f6d7b127c6318
Berzan <berzan@tutanota.com>
2022-07-17 13:11:10 +0300

    require: styles.css

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+require('@solana/wallet-adapter-react-ui/styles.css')
 import type { AppProps } from 'next/app'
 import { Page } from '../components/Common'
 import { TopBar } from '../components/TopBar'

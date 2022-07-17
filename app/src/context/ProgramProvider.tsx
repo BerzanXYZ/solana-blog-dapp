9fcfcfcf4719d05f8f68f448234b25aee72e322d
Berzan <berzan@tutanota.com>
2022-07-17 19:01:11 +0300

    add: blogs array

--- app/src/context/ProgramProvider.tsx
+++ app/src/context/ProgramProvider.tsx
@@ -2,12 +2,13 @@ import { Program, Wallet } from "@project-serum/anchor";
 import { useAnchorWallet, useConnection, useWallet } from "@solana/wallet-adapter-react";
 import { Keypair } from "@solana/web3.js";
 import { createContext, ReactNode, useContext, useEffect, useMemo, useRef, useState } from "react";
-import { getProgram, createBlogOnProgram, makePostOnProgram } from "../utils/program";
+import { getProgram, createBlogOnProgram, makePostOnProgram, getAccountsOnProgram, BlogAccount } from "../utils/program";
 import { SolanaBlogDapp } from "../utils/solana-blog-dapp";
 
 interface ProgramContextState {
     createBlog(author: string, blog: string): Promise<void>,
     makePost(post: string): Promise<void>,
+    blogs: BlogAccount[]
 }
 
 const ProgramContext = createContext({} as ProgramContextState)
@@ -17,6 +18,7 @@ export function useProgram() {
 }
 
 export const ProgramProvider = ({ children }: { children: ReactNode }) => {
+    const [blogs, setBlogs] = useState<BlogAccount[]>([])
     const wallet = useWallet()
     const anchorWallet = useAnchorWallet()
     const { connection } = useConnection()
@@ -37,8 +39,18 @@ export const ProgramProvider = ({ children }: { children: ReactNode }) => {
         await makePostOnProgram(program.current, post, wallet.publicKey)
     }
 
+    async function getBlogs() {
+        if(!wallet.publicKey) return
+        setBlogs(await getAccountsOnProgram(program.current))
+    }
+
+    useEffect(() => {
+        if(!wallet.publicKey) return
+        getBlogs()
+    }, [wallet.publicKey])
+
     return (
-        <ProgramContext.Provider value={{createBlog, makePost}}>
+        <ProgramContext.Provider value={{createBlog, makePost, blogs}}>
             {children}
         </ProgramContext.Provider>
     )

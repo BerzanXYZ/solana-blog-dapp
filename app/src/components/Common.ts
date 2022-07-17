634339f3d5e9c2c6f44f73f913ca5b627535f3e2
Berzan <berzan@tutanota.com>
2022-07-17 12:58:15 +0300

    change: style

--- app/src/components/Common.ts
+++ app/src/components/Common.ts
@@ -4,6 +4,6 @@ export const Page = styled.div`
     display: flex;
     flex-direction: column;
     min-height: 100vh;
-    color: #000000;
-    background: #14F195;
+    color: white;
+    background: linear-gradient(45deg, rgba(153,69,255,1) 0%, rgba(20,241,149,1) 100%);
 `

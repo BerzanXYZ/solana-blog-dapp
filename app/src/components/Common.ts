ccafb0742a087a373a070f4945480c6c8b418fd3
Berzan <berzan@tutanota.com>
2022-07-17 12:34:46 +0300

    add: Common components

---
+++ app/src/components/Common.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Page = styled.div`
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    color: #000000;
+    background: #14F195;
+`

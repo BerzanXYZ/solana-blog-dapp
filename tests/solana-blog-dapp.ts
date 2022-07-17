44501881a121ac36c107c37798bd95bb61e031df
Berzan <berzan@tutanota.com>
2022-07-17 10:29:38 +0300

    add: makePost method test

--- tests/solana-blog-dapp.ts
+++ tests/solana-blog-dapp.ts
@@ -4,13 +4,44 @@ import { SolanaBlogDapp } from "../target/types/solana_blog_dapp";
 
 describe("solana-blog-dapp", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env()
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.SolanaBlogDapp as Program<SolanaBlogDapp>;
 
   it("Can create a Blog Account!", async () => {
-    // Add your test here.
-    const tx = await program.methods.createBlog().rpc();
-    console.log("Your transaction signature", tx);
+    // Create a new keypair to use as a blog account
+    const kp = anchor.web3.Keypair.generate()
+
+    // Create a new Blog Account
+    await program.methods.createBlog().accounts({
+      blogAccount: kp.publicKey,
+      signer: provider.wallet.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId,
+    }).signers([kp]).rpc();
+
   });
+
+
+  it("Can make a post!", async () => {
+    // Create a new keypair to use as a blog account
+    const kp = anchor.web3.Keypair.generate()
+
+    // Create a new Blog Account
+    await program.methods.createBlog().accounts({
+      blogAccount: kp.publicKey,
+      signer: provider.wallet.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId,
+    }).signers([kp]).rpc();
+
+    // Make a post
+    await program.methods.makePost(Buffer.from("Hello, blog!")).accounts({
+      author: provider.wallet.publicKey,
+      blogAccount: kp.publicKey,
+    }).rpc()
+
+    // Fetch blog account
+    const account = await program.account.blogAccount.fetch(kp.publicKey)
+    console.log('Author:', account.author.toBase58(), '\nPost:', (account.latestPost as Buffer).toString());
+  })
 });

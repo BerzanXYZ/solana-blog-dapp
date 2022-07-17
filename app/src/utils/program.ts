c2a64469525529f2f9cd75b546532cb5febe2261
Berzan <berzan@tutanota.com>
2022-07-17 14:31:55 +0300

    add: program.ts

---
+++ app/src/utils/program.ts
@@ -0,0 +1,44 @@
+import { AnchorProvider, Program, Wallet } from "@project-serum/anchor";
+import { PublicKey, SystemProgram, Connection, ConfirmOptions, Keypair } from "@solana/web3.js";
+import { getBlogAccount } from "./blogAccount";
+import {IDL, type SolanaBlogDapp} from "./solana-blog-dapp"
+
+
+export function getProgram(connection: Connection) {
+    const provider = new AnchorProvider(connection, new Wallet(getBlogAccount()), 'confirmed' as ConfirmOptions)
+    const programId = new PublicKey('8vn9kdHuybwnWiwQDc16XKjUvsvKxwmjA53uKAtXTbpk')
+    return new Program(IDL, programId, provider)
+}
+
+
+export async function createBlogOnProgram(program: Program<SolanaBlogDapp>, author_name: string, blog_name: string, author: PublicKey) {
+    // Get blog account from local storage
+    const blogAccount = getBlogAccount()
+    // Make a tx to create a blog
+    const tx = await program.methods.createBlog(author_name, blog_name)
+    .accounts({
+        blogAccount: blogAccount.publicKey,
+        systemProgram: SystemProgram.programId,
+        author: author,
+    }).signers([blogAccount])
+    .rpc()
+    
+    console.log(tx);
+}
+
+export async function makePostOnProgram(program: Program<SolanaBlogDapp>, new_post: string, author: PublicKey) {
+    // Get blog account from local storage
+    const blogAccount = getBlogAccount()
+    // Convert string into utf8 encoded buffer
+    const post_utf8_encoded = Buffer.from(new_post)
+
+    // Make a tx to make a post
+    const tx = await program.methods.makePost(post_utf8_encoded)
+    .accounts({
+        blogAccount: blogAccount.publicKey,
+        author: author
+    }).signers([blogAccount])
+    .rpc()
+    
+    console.log(tx)
+}

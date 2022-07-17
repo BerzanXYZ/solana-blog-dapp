b42da1a07d1e6985a0b1943a126c2d93c40064b7
Berzan <berzan@tutanota.com>
2022-07-17 14:50:42 +0300

    add: Main component

--- app/src/components/Common.ts
+++ app/src/components/Common.ts
@@ -3,7 +3,16 @@ import styled from "styled-components";
 export const Page = styled.div`
     display: flex;
     flex-direction: column;
+    align-items: center;
     min-height: 100vh;
     color: white;
     background: rgba(20,241,149,1);
+`
+
+export const Main = styled.main`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    max-width: 1024px;
+    padding: 1rem;
 `
--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,16 +1,19 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { Main } from '../components/Common'
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <>
       <Head>
         <title>Solana Blog dApp</title>
         <meta name="description" content="A Blog dApp on Solana brought you by Berzan." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-    </div>
+      <Main>
+        Hello
+      </Main>
+    </>
   )
 }
 
